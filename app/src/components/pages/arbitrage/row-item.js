e9c1184b023f727dc15cbd1bb3a590305d07b225
bahgerani <mohammad_bagherani@yahoo.com>
2021-12-08 17:41:16 +0330

    fav icon updated, date-formate changed in arb

--- app/src/components/pages/arbitrage/row-item.js
+++ app/src/components/pages/arbitrage/row-item.js
@@ -4,37 +4,10 @@ import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 
 function ArbitrageListItem({ Token0Symbol, Date: date, Token1Symbol, Percent, BuyIn, SellIn, MinRatio, MaxRatio, logoAddress }) {
-  const wrapTimeSince = (val, str) => {
-    return <span className="text-warning">{val}<span className="text-small">{str}</span></span>;
-  };
 
   const timeSince = (date) => {
-
-    var seconds = Math.floor((new Date() - date) / 1000);
-
-    var interval = seconds / 31536000;
-
-    if (interval > 1) {
-      return wrapTimeSince(Math.floor(interval), 'years');
-    }
-    interval = seconds / 2592000;
-    if (interval > 1) {
-      return wrapTimeSince(Math.floor(interval), 'months');
-    }
-    interval = seconds / 86400;
-    if (interval > 1) {
-      return wrapTimeSince(Math.floor(interval), ' days');
-    }
-    interval = seconds / 3600;
-    if (interval > 1) {
-      return wrapTimeSince(Math.floor(interval), ' hours');
-    }
-    interval = seconds / 60;
-    if (interval > 1) {
-      return wrapTimeSince(Math.floor(interval), ' minutes');
-    }
-
-    return wrapTimeSince(Math.floor(seconds), ' seconds');
+    let d = new Date(date);
+    return (<span className="text-warning">{d.toLocaleDateString('en-UK')}&nbsp;{d.toLocaleTimeString('en-UK')}</span>);
   };
 
   const renderTooltip = ({ date, ...props }) => (
